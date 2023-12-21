8a666ca98a8a914b51921f61383d51f7f413fc0d
carmeliomri <carmeli.omri@gmail.com>
2023-12-21 20:06:31 +0200

    App runs, 2 text boxes, all four '+' '-' '*' '/' buttons are clicking, a result textview box. App completed, all calculations work.

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onBtnClicked(View view) {
+        // Get view handle using the view id
+        EditText et1 = findViewById(R.id.Num1);
+        EditText et2 = findViewById(R.id.Num2);
+
+// Get the view’s text
+        String et1Text = et1.getText().toString();
+        String et2Text = et2.getText().toString();
+// Cast the String into Integer
+        Integer num1 = Integer.valueOf(et1Text);
+        Integer num2 = Integer.valueOf(et2Text);
+        Integer result = null;
+        if (view.getId() == R.id.btnPlus)
+            result = num1 + num2;
+        if (view.getId() == R.id.btnMinus)
+            result = num1 - num2;
+        if (view.getId() == R.id.btnMult)
+            result = num1 * num2;
+        if (view.getId() == R.id.btnDiv)
+            result = num1 / num2;
+        if (result != null) {
+            TextView tvRes = findViewById(R.id.tvResult);
+            tvRes.setText(result.toString());
+        }
+
     }
 }
